6a8de99a18fd2f3a0ba4bdb95851ed460235bcaf
sistemas <sistemas@VRE_LAB0302.inacap.cl>
2016-07-03 21:22:33 -0300

    Mensaje de error al no poner nombres

--- BecarRuzFSaavedra_prueba4-war/src/java/cl/beans/RecepcionBean.java
+++ BecarRuzFSaavedra_prueba4-war/src/java/cl/beans/RecepcionBean.java
@@ -20,6 +20,7 @@ import java.util.List;
 import java.util.Map;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
